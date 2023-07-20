664a8324140548716e1d7e9154a02de4af64c822
Andras Bacsai <3135342+mychiefs58js@users.noreply.github.com>
2023-07-20 12:01:28 +0200

    update versions

--- services/scripts/split.mjs
+++ services/scripts/split.mjs
@@ -1,21 +1,21 @@
-import fs from 'fs/promises';
-import yaml from 'js-yaml';
-import got from 'got';
+import fs from "fs/promises";
+import yaml from "js-yaml";
+import got from "got";
 
 async function getRemote() {
-    const templateDir = 'https://github.com/coollabsio/coolify/raw/next/apps/api/devTemplates.yaml'
-    return await got.get(templateDir).text()
+  const templateDir =
+    "https://github.com/coollabsio/coolify/raw/v3/apps/api/devTemplates.yaml";
+  return await got.get(templateDir).text();
 }
 async function getLocal() {
-    const templateDir = '../coolify-v3/apps/api/devTemplates.yaml'
-    return await fs.readFile(templateDir, 'utf8')
+  const templateDir = "../coolify-v3/apps/api/devTemplates.yaml";
+  return await fs.readFile(templateDir, "utf8");
 }
 const data = await getRemote();
-const json = yaml.load(data)
+const json = yaml.load(data);
 for (const template of json) {
-    const type = template.type
-    const file = `./services/templates/${type}.yaml`
-    const data = yaml.dump(template)
-    await fs.writeFile(file, data)
+  const type = template.type;
+  const file = `./services/templates/${type}.yaml`;
+  const data = yaml.dump(template);
+  await fs.writeFile(file, data);
 }
-
